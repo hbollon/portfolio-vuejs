289bb63e0f1f74fd483b515f97f3af711a59ce2f
390168 <hyebee.lee@iljin.co.kr>
2022-05-02 14:49:25 +0900

    Updates

--- src/cosmic.js
+++
@@ -1,8 +0,0 @@
-import Cosmic from "cosmicjs";
-
-const cosmic = Cosmic(process.env.VUE_APP_COSMICJS_TOKEN);
-export const bucket = cosmic.bucket({
-  slug: process.env.VUE_APP_COSMICJS_BUCKET_SLUG,
-  read_key: process.env.VUE_APP_COSMICJS_BUCKET_READ_KEY,
-  write_key: "",
-});
